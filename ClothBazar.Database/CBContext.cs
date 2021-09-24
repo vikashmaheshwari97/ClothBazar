fa43d6c8e59c2ac73418d9b2561e5d134a2ae57c
Vishu Maheshwari <vickymaheshwari192@gmail.com>
2021-09-24 18:24:43 +0500

    Add project files.

---
+++ ClothBazar.Database/CBContext.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+using ClothBazar.Entities;
+
+namespace ClothBazar.Database
+{
+    public class CBContext : DbContext
+    {
+        public CBContext(): base("ClothBazarConnection")
+        {
+
+        }
+
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Product> Products  { get; set; }
+    }
+
+}
